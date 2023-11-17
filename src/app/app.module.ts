e073075cb5b36ccd9fc7ccb681079bed8fed7a2e
cpierres <cpierres@hotmail.com>
2023-11-17 15:09:57 +0100

    Initialisation nouveau composant demobinding (+ création script dans package.json)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DemobindingComponent } from './demobinding/demobinding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemobindingComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/demobinding/demobinding.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demobinding',
+  templateUrl: './demobinding.component.html',
+  styleUrls: ['./demobinding.component.css']
+})
+export class DemobindingComponent {
+
+}
