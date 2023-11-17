d38c996c1f19413a733914134d964bf3e986e0f4
cpierres <cpierres@hotmail.com>
2023-11-17 18:13:13 +0100

    Branchement des composants aux options de menu déroulant

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {ProduitsComponent} from "./produits/produits.component";
 import {DemobindingComponent} from "./demobinding/demobinding.component";
+import {AddproduitComponent} from "./addproduit/addproduit.component";
 
 const routes: Routes = [
   {path: "produits", component: ProduitsComponent},
-  {path:"demobinding",component:DemobindingComponent}
+  {path:"demobinding",component:DemobindingComponent},
+  {path: "add-produit", component: AddproduitComponent},
 ];
 
 @NgModule({
