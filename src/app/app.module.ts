e499a67a5efa5b0ec4ccc39b9a4b684a78803513
cpierres <cpierres@hotmail.com>
2023-11-17 17:00:15 +0100

    Initialisation composant produits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { DemobindingComponent } from './demobinding/demobinding.component';
 
 import {FormsModule} from "@angular/forms";//pour 2 way binding
+import { ProduitsComponent } from './produits/produits.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemobindingComponent
+    DemobindingComponent,
+    ProduitsComponent
   ],
   imports: [
     BrowserModule,
